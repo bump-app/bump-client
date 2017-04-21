94cf2aad8e4941725eb813ded0da2e01d356286c
matthewdias <matthewdias@me.com>
2017-04-21 00:49:22 -0500

    Fix channel-nav active

--- app/routes/channels/all.js
+++ app/routes/channels/all.js
@@ -5,7 +5,7 @@ import set from 'ember-metal/set';
 export default Route.extend({
   model() {
     const channelController = this.controllerFor('channels');
-    set(channelController, 'currentChannel', 'all');
+    set(channelController, 'channelName', 'all');
     return get(this, 'store').findAll('channel');
   }
 });
