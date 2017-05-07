fb424b7ad247d14129bb82374b55f48ccb9bc73c
William Lin <wlsaidhi@gmail.com>
2017-05-06 19:38:58 -0500

    added registration and login with validations

--- app/models/user.js
+++ app/models/user.js
@@ -1,8 +1,44 @@
 import Model from 'ember-data/model';
 import attr from 'ember-data/attr';
 import { hasMany } from 'ember-data/relationships';
+import { buildValidations, validator } from 'ember-cp-validations';
 
-export default Model.extend({
-  name: attr('string'),
+const Validations = buildValidations({
+    first_name: validator('presence', true),
+
+    last_name: validator('presence', true),
+
+    email: [
+        validator('presence', true),
+        validator('format', {
+            lazy: false,
+            type: 'email',
+            message: 'Email is not valid'
+        }),
+        validator('email-available', { debounce: 500 })
+    ],
+
+    password: [
+        validator('presence', true),
+        validator('length', {
+            min: 4
+        })
+    ],
+
+    confirm_password: [
+        validator('presence', true),
+        validator('confirmation', {
+            on: 'password',
+            message: 'Passwords do not match'
+        })
+    ], 
+});
+
+export default Model.extend(Validations, {
+  first_name: attr('string'),
+  last_name: attr('string'),
+  email: attr('string'),
+  password: attr('string'),
+  /*confirm_password: attr('string'),*/
   posts: hasMany('post'),
 });
