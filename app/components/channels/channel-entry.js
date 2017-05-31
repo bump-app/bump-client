239302dd7dd3dd9e2d65148115e41bb372c96052
matthewdias <matthewdias@me.com>
2017-05-31 15:19:06 -0500

    Cleanup codestyle

--- app/components/channels/channel-entry.js
+++ app/components/channels/channel-entry.js
@@ -1,22 +1,21 @@
 import Ember from 'ember';
-import get, { getProperties } from 'ember-metal/get';
+import get from 'ember-metal/get';
 import service from 'ember-service/inject';
 
 export default Ember.Component.extend({
-    classNames: ['channel-entry'],
-    attributeBindings: ['channel'],
-    store: service(),
-    currentUser: service(),
-    actions: {
-        subscribe(channel) {
-            get(this, 'currentUser').getuser().then(user => {
-                const store = get(this, 'store');
-                const subscription = store.createRecord('subscription', { user, channel });
-                subscription.save();
-                /*this.transitionToRoute('discover');*/
-            });
+  classNames: ['channel-entry'],
+  attributeBindings: ['channel'],
+  store: service(),
+  currentUser: service(),
 
-        }
+  actions: {
+    subscribe(channel) {
+      get(this, 'currentUser').getuser().then(user => {
+        const store = get(this, 'store');
+        const subscription = store.createRecord('subscription', { user, channel });
+        subscription.save();
+        /*this.transitionToRoute('discover');*/
+      });
     }
-
+  }
 });
--- app/controllers/register.js
+++ app/controllers/register.js
@@ -1,18 +1,18 @@
 import Ember from 'ember';
-import get, { getProperties } from 'ember-metal/get';
+import get from 'ember-metal/get';
 
 export default Ember.Controller.extend({
-    actions: {
-        register() {
-            const user = get(this, 'model');
+  actions: {
+    register() {
+      const user = get(this, 'model');
 
-            user.validate()
-                .then(({ validations }) => {
-                    if (validations.get('isValid')) {
-                        user.save();
-                        this.transitionToRoute('index')
-                    }
-                });
-        }
+      user.validate()
+        .then(({ validations }) => {
+          if (validations.get('isValid')) {
+            user.save();
+            this.transitionToRoute('index')
+          }
+        });
     }
+  }
 });
