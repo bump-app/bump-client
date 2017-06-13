d255994acf57ffb5bc96b5b769b6dd7dc293908c
matthewdias <matthewdias@me.com>
2017-06-13 01:01:56 -0500

    Design auth flow, refactor validation

--- app/controllers/sign-up.js
+++ app/controllers/sign-up.js
@@ -1,18 +1,21 @@
 import Controller from 'ember-controller';
-import get from 'ember-metal/get';
+import get, { getProperties } from 'ember-metal/get';
 
 export default Controller.extend({
   actions: {
     register() {
       const user = get(this, 'model');
 
-      user.validate()
-        .then(({ validations }) => {
-          if (validations.get('isValid')) {
-            user.save();
-            // this.transitionToRoute('index')
-          }
-        });
+      user.validate().then(({ validations }) => {
+        if (get(validations, 'isValid')) {
+          user.save().then(() => {
+            const { email, password } = getProperties(user, 'email', 'password');
+            get(this, 'session').authenticateWithOAuth2(email, password).then(() => {
+              this.transitionToRoute('dashboard.all');
+            });
+          });
+        }
+      });
     }
   }
 });
