ea90907f9bf6ec0fd0193eac141e3de0af34cddc
William Lin <wlsaidhi@gmail.com>
2017-07-02 03:07:29 -0500

    switched to ember-concurrency tasks for sub and unsub

--- app/components/channels/channel-entry.js
+++ app/components/channels/channel-entry.js
@@ -1,6 +1,7 @@
 import Component from 'ember-component';
 import get from 'ember-metal/get';
 import service from 'ember-service/inject';
+import { task } from 'ember-concurrency';
 
 export default Component.extend({
   classNames: ['channel-entry'],
@@ -16,30 +17,28 @@ export default Component.extend({
     this._super(...arguments);
   },
 
-  actions: {
-    subscribe(channel) {
-      const user = get(this, 'session.account')
-      const store = get(this, 'store');
-      const subscription = store.createRecord('subscription', { user, channel });
-      subscription.save().then(() => {
-        this.set('subscriptions', get(this, 'store').query('subscription', {
-          filter: { userId: get(this, 'session.account.id'),
-            channelId: get(channel, 'id') }
-        }));
-      });
-      get(this, 'router').transitionTo('dashboard.all', channel);
-    },
+  subscribe: task(function * (channel) {
+    const user = get(this, 'session.account');
+    const store = get(this, 'store');
+    const subscription = store.createRecord('subscription', { user, channel });
 
-    unsubscribe(channel) {
-      get(this, 'store').query('subscription', {
-        filter: { userId: get(this, 'session.account.id'),
-                  channelId: get(channel, 'id') }
-      }).then((subscriptions) =>
-        subscriptions.forEach((subscription) => subscription.destroyRecord()));
-      this.set('subscriptions', get(this, 'store').query('subscription', {
-        filter: { userId: get(this, 'session.account.id'),
-          channelId: get(channel, 'id') }
-      }));
-    }
-  }
+    yield subscription.save();
+    this.set('subscriptions', get(this, 'store').query('subscription', {
+      filter: { userId: get(this, 'session.account.id'),
+        channelId: get(channel, 'id') }
+    }));
+    get(this, 'router').transitionTo('dashboard.all', channel);
+  }).drop(),
+
+  unsubscribe: task(function * (channel) {
+    let subscriptions = yield get(this, 'store').query('subscription', {
+      filter: { userId: get(this, 'session.account.id'),
+        channelId: get(channel, 'id') }
+    });
+    yield subscriptions.forEach((subscription) => subscription.destroyRecord());
+    this.set('subscriptions', get(this, 'store').query('subscription', {
+      filter: { userId: get(this, 'session.account.id'),
+        channelId: get(channel, 'id') }
+    }));
+  }).drop()
 });
