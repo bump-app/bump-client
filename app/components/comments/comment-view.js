f6929c2883694e7d7efe5fd415b93dd95ff8b666
William Lin <wlsaidhi@gmail.com>
2017-07-16 01:09:30 -0500

    users can now delete and edit their comments

--- app/components/comments/comment-view.js
+++ app/components/comments/comment-view.js
@@ -1,5 +1,20 @@
 import Component from 'ember-component';
+import get from 'ember-metal/get';
+import computed from 'ember-computed';
 
 export default Component.extend({
-  classNames: ['comment-view']
+  classNames: ['comment-view'],
+  editing: false,
+
+  owner: computed('session', 'comment', function() {
+    return get(this, 'session.account.id') == get(this, 'comment.user.id');
+  }).readOnly(),
+  
+  actions: {
+    delete_comment(comment) {
+      // add confirmation popup?
+      comment.destroyRecord();
+    }
+  }
+
 });
